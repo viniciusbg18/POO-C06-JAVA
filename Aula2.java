eb3b1cfd04260e420684400d7ccf8b71bf5d4f6d
viniciusbg18 <viniciusbaganhagodoy@gmail.com>
2026-02-23 20:30:38 -0300

    Aula 2
    Aula do dia 23 de fevereiro de 2026

---
+++ Aula2.java
@@ -0,0 +1,12 @@
+public class Aula2 {
+    public static void main(String[] args) {
+        int horas = 3; 
+        int primera_hora = 10;
+        int segunda_hora = 4;
+        int terceira_hora = 2;
+        int total_horas = primera_hora + segunda_hora + terceira_hora;
+        double media = total_horas / horas;
+        System.out.println("Total de horas: " + total_horas);
+        System.out.println("Média de horas: " + media);
+    }
+}

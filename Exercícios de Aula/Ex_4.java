4d9eb0c78f834586481fc3076038a442363741f7
viniciusbg18 <viniciusbaganhagodoy@gmail.com>
2026-03-02 20:14:53 -0300

    Update Ex_4.java

--- Exercícios de Aula/Ex_4.java
+++ Exercícios de Aula/Ex_4.java
@@ -10,6 +10,7 @@ public class Ex_4 {
             System.out.println("O aluno está aprovado!");
         } else if(npa >= 30 && npa < 60){;
             System.out.println("O aluno terá que fazer NP3!");
+            System.out.println("Entre com a nota da NP3: ");
             int np3 = teclado.nextInt();
             nfa = (npa + np3) / 2;
             if (nfa >= 50) {
